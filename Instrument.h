d02c254c0e5c84690b69c5ccdec5bd356b8c6597
Luis Gaytan <a01771191@tec.mx>
2025-06-10 23:43:07 -0600

    Update Instrument.h

--- Instrument.h
+++ Instrument.h
@@ -9,11 +9,12 @@ class Instrument {
 protected:
     std::string name;
     int volume;
-    std::vector<Effect*> effects; // effects list
+    int duration;  // duration of the instrument in the song
+    std::vector<Effect*> effects; // effects applied to the instrument
 
 public:
     Instrument(); // default constructor
-    Instrument(const std::string& name, int volume); // overloaded constructor
+    Instrument(const std::string& name, int volume, int duration); // overloaded constructor
 
     // get n set
     std::string getName() const;
@@ -22,11 +23,20 @@ public:
     int getVolume() const;
     void setVolume(int volume);
 
-    // virtual methods
-    virtual std::string playSound() const = 0; // abstract
+    int getDuration() const;
+    void setDuration(int duration);
+
+    // abstract methods
+    virtual std::string playSound() const = 0;   // abstract method
+    virtual std::string playSound(int volume) const; // method overloading
     void addEffect(Effect* effect);
     void removeEffect(Effect* effect);
     void clearEffects();
+
+    // operator overloading
+    Instrument* operator+(const Instrument& other); // combine two instruments
+    Effect* operator[](int index); // access effects by index
+    friend std::ostream& operator<<(std::ostream& os, const Instrument& instrument);  // print instrument
 };
 
 #endif
