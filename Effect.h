fe84b24f8d1c8b7097d2ac907e3716ba8f237f12
Luis Gaytan <a01771191@tec.mx>
2025-06-10 23:44:27 -0600

    Update Effect.h

--- Effect.h
+++ Effect.h
@@ -15,8 +15,8 @@ public:
     std::string getEffectName() const;
     void setEffectName(const std::string& name);
 
-    // virtual method
-    virtual std::string modifySound(const std::string& sound) const = 0; // abstract
+    // abstract method
+    virtual std::string modifySound(const std::string& sound) const = 0; // abstract method
 };
 
 #endif
