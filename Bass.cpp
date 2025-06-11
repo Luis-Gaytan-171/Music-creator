1cbdd7a2e0ed3fac395c3e8abdce6b8e52648a2e
Luis Gaytan <a01771191@tec.mx>
2025-06-10 23:47:37 -0600

    Update Bass.cpp

--- Bass.cpp
+++ Bass.cpp
@@ -1,18 +1,16 @@
 #include "Bass.h"
 
 // constructors
-Bass::Bass() : Instrument("Bass", 50) {}
+Bass::Bass() : Instrument("Bass", 50, 0) {}
 
-Bass::Bass(const std::string& name, int volume) : Instrument(name, volume) {}
+Bass::Bass(const std::string& name, int volume, int duration) : Instrument(name, volume, duration) {}
 
-// override
+// overloading the playSound method
 std::string Bass::playSound() const {
-    std::string sound = "Deep bass sound";
+    return "Deep bass sound";
+}
 
-    // apply effects
-    for (const auto& effect : effects) {
-        sound = effect->modifySound(sound);
-    }
-
-    return sound;
+// overloading the playSound method with volume
+std::string Bass::playSound(int volume) const {
+    return "Deep bass sound at volume " + std::to_string(volume);
 }
